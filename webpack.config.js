519bbb136b3411c72b7501249fc433c7702f6be9
Sean Collings <nightharrier@gmail.com>
2022-07-07 15:25:08 +0200

    update logger export file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   target: 'node',
   externals: [nodeExternals()],
-  entry: './src/index.ts',
+  entry: './src/logger.ts',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js',
